3a5aa1802c15c0101ff9cdb7cd2afffbab96330f
Adam Goarley <agoarley@icloud.com>
2019-11-04 17:22:44 -0500

    updates, db still broken

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,36 @@ router.get('/', (req, res) => {
         console.log(result); // should see objects wrapped in an array
 
         // render the home view with dynamic data
-        res.render('home', { data: result });
+         res.render('home', { people: result });
+
+        //  
     })
 })
+
+    router.get('/user/id/:id', (req, res) => {
+        console.log('hit a dynamic route!');
+        console.log(req.params.id);
+
+        let query = `SELECT * FROM tbl_bio WHERE profID="${req.params.id}"`;
+
+            sql.query(query, (err, result) => {
+                if (err) { throw err; console.log(err); }
+        
+                //console.log(result[0].social); // should see objects wrapped in an array
+        
+                // turn our social property into an array - its just text in the database
+                // which isnt really anything we can work with
+        //         result[0].social = result[0].social.split(",").map(function(item){
+        //             item = item.trim(); //remove the extra space from each word
+        
+        //             return item;
+        //         })
+        
+                // send the DB query back to the browser
+                res.json(result);
+        })
+
+        
+    })
 
 module.exports = router;
